086de3840020c1a7ec33c7f8e1832475e98bfb39
Gaurav Goel <gaurav.int@otomashen.com>
2026-02-03 20:11:46 +0530

    hello world

---
+++ app.py
@@ -0,0 +1,9 @@
+print("Hello world")
+# n=9
+# def poweroftwo(n):
+#     if n==0:
+#         return 1
+#     else:
+#         power = poweroftwo(n-1)
+#         return power*2
+# print(poweroftwo(n))
